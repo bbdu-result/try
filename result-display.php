96eccfa6ef1cc3ea3fb7681f7e7380219d2b3847
bbdu-result <140169425+bbdu-result@users.noreply.github.com>
2023-07-25 12:28:44 +0530

    Add files via upload

---
+++ result-display.php
@@ -0,0 +1,317 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="C:\Users\Asus\Desktop\trying\103.85.141.2268063\bbdu-result\css\full-slider.css" rel="stylesheet">
+<title>Print Form</title>
+<style type="text/css">
+
+.tb-pnt {
+  font-family: Verdana, Geneva, sans-serif;
+}
+.tb-mrsheet tr >td {
+  border: none;
+  padding:0;
+}
+.tb-mrsheet {
+  border: none;
+}
+.tb-mrsheet tr > td >.table tr > td {
+  padding:3px 5px;
+  border-right: solid 1px #dee2e6;
+  border-bottom:solid 1px #000;
+}
+
+.tb-print .table thead th {
+  font-size: 13px;
+  text-align: center;
+  font-weight: normal;
+  padding:3px 0;
+  vertical-align:middle;
+}
+.hdr > h2 {
+  text-align: center;
+  margin-bottom: 30px;
+  font-family:"Times New Roman", Times, serif;
+}
+.hdr > h2 > span {
+  font-size: 28px;
+  font-weight:bold;
+  font-family:Verdana, Geneva, sans-serif;
+}
+.hdr > h2 > span > b {
+  font-size: 18px;
+  display:block;
+}
+
+.ft.container p {
+  margin-bottom: 0;
+}
+.bdr-line {
+  border-bottom: solid 2px #000;
+  margin: 0;
+  padding: ;
+}
+
+.table.tb-mrsheet thead th,
+.table.tb-mrsheet tbody td,
+.table-bordered {border-color:#000!important;}
+.table-bordered thead td, .table-bordered thead th{border-bottom-width:1px;}
+.table-bordered thead td, .table-bordered thead th{border-left:none;}
+
+
+p {
+
+  margin-bottom: 0rem;
+}
+</style>
+</head>
+<body class="tb-pnt">
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12  hdr">
+      <div style="text-align:center;"> <span style="font-weight:bold; font-size:30px ;" >Babu Banarasi Das University, Lucknow </sapn><br> <span style="font-size:24px; font-weight:bold ;">School of Engineering</span><br>  <span style="font-size: 17px; font-weight: bold;">Bachelor of Technology</span> 
+        <br>  <span style="font-size: 14px;">Second Year Session: 2022-23</span>
+      </div>
+    </div>
+    <div class="col-sm-12 bd">
+      <div class="row">
+        <div class="col-sm-6">
+          <p>Name: <b>TUSHAR PANDEY</b></p>
+        </div>
+        <div class="col-sm-3" >
+          <p>Roll No: <b>1210432345</b></p>
+        </div>
+        <div class="col-sm-3" >
+          <p>Status: <b>MAIN</b></p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">
+          <p>Father Name: <b>A K PANDEY</b></p>
+        </div>
+        <div class="col-sm-6" >
+          <p>Enrollment No: <b>12104320417</b></p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">
+          <p>Mother Name: <b>MAMTA PANDEY</b></p>
+        </div>
+        <div class="col-sm-6" >
+          <p>Branch: <b>Computer Science & Engineering</b></p><br><br>
+        </div>
+      </div>
+                   <table class="table tb-print table-bordered tb-mrsheet" style="margin-bottom: 0px; border-collapse:collapse; text-align:center;">
+        <tr>
+          <td>
+          <table class="table table-bordered" style="margin-bottom: 0px; border-collapse:collapse" >
+              <thead>
+                <tr>
+                  <th colspan="9"width="10%">Third Semester</th>
+                </tr>
+                <tr>
+                 <th rowspan="2">Subject Code</th><th colspan="2">ESE Marks</th><th colspan="2">CIA Marks</th><th colspan="2">Total</th> 
+                                  <th rowspan="2">Credit</th><th rowspan="2">Grade</th>                 </tr>
+                <tr>
+                  <th>Obt.</th><th>Max.</th> <th>Obt.</th><th>Max.</th> <th>Obt.</th><th>Max.</th>
+                </tr>
+              </thead>
+              <tbody>
+                              <tr>
+                  <td>BAS3301</td><td>23</td><td>60</td><td>25</td><td>40</td><td> 48</td><td>100</td>
+                  <td>4</td><td>C</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3301</td><td>24</td><td>60</td><td>26</td><td>40</td><td> 50</td><td>100</td>
+                  <td>4</td><td>B</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3302</td><td>24</td><td>60</td><td>26</td><td>40</td><td> 50</td><td>100</td>
+                  <td>3</td><td>B</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3303</td><td>08</td><td>60</td><td>22</td><td>40</td><td> 30</td><td>100</td>
+                  <td>0</td><td>F</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3305</td><td>27</td><td>60</td><td>22</td><td>40</td><td> 49</td><td>100</td>
+                  <td>4</td><td>C</td>
+                                  </tr>
+                                <tr>
+                  <td>BHS3302</td><td>35</td><td>60</td><td>25</td><td>40</td><td> 60</td><td>100</td>
+                  <td>2</td><td>A</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3351</td><td>46</td><td>60</td><td>32</td><td>40</td><td> 78</td><td>100</td>
+                  <td>1</td><td>A+</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3352</td><td>54</td><td>60</td><td>32</td><td>40</td><td> 86</td><td>100</td>
+                  <td>1</td><td>A+</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3353</td><td>52</td><td>60</td><td>26</td><td>40</td><td> 78</td><td>100</td>
+                  <td>1</td><td>A+</td>
+                                  </tr>
+                                <tr>
+                  <td>BCS3355</td><td>47</td><td>60</td><td>27</td><td>40</td><td> 74</td><td>100</td>
+                  <td>1</td><td>A</td>
+                                  </tr>
+                                <tr>
+                  <td>GP3301</td><td></td><td></td><td>73</td><td>100</td><td> 73</td><td>100</td>
+                  <td>1</td><td>A</td>
+                                  </tr>
+                               <tr style="font-weight:bold;">
+                  <td>Total</td><td>340</td><td>600</td><td>336</td><td>500</td><td> 676</td><td>1100</td><td></td><td></td>                </tr>
+                                <tr style="text-align:center">
+                  <td colspan="3">Credit Theory</td><td colspan="3">Credit Practical</td><td colspan="3" rowspan="2">SGPA</td>
+                </tr>
+                <tr style="text-align:center">
+                  <td colspan="2">Obt.</td><td>Max</td> <td colspan="2">Obt.</td><td>Max</td>
+                </tr>
+                <tr style="text-align:center">
+                  <td colspan="2">17</td><td>21</td> <td colspan="2"> 5</td><td> 5</td>
+                  <td colspan="3">5.42</td>
+                </tr>
+                                  </tbody>
+            </table></td>
+          <td>
+                        <table class="table table-bordered"  style="border-left:none;margin-bottom: 0px; ">
+               <thead>
+                 <tr>
+                  <th colspan="9"width="10%">Fourth Semester</th>
+                </tr>
+                <tr>
+                 <th rowspan="2">Subject Code</th><th colspan="2">ESE Marks</th><th colspan="2">CIA Marks</th><th colspan="2">Total</th> <th rowspan="2">Credit</th><th rowspan="2">Grade</th>                 </tr>
+                <tr>
+                  <th>Obt.</th><th>Max.</th> <th>Obt.</th><th>Max.</th> <th>Obt.</th><th>Max.</th>
+                </tr>
+              </thead>
+              <tbody>
+                              <tr>
+                  <td>BAS3401</td><td>24</td><td>60</td><td>24</td><td>40</td><td>48</td><td>100</td><td>3</td><td>C</td>                </tr>
+                                <tr>
+                  <td>BCS3401</td><td>33</td><td>60</td><td>26</td><td>40</td><td>59</td><td>100</td><td>4</td><td>B+</td>                </tr>
+                                <tr>
+                  <td>BCS3402</td><td>31</td><td>60</td><td>24</td><td>40</td><td>55</td><td>100</td><td>4</td><td>B+</td>                </tr>
+                                <tr>
+                  <td>BCS3403</td><td>18</td><td>60</td><td>24</td><td>40</td><td>42</td><td>100</td><td>4</td><td>P</td>                </tr>
+                                <tr>
+                  <td>BCS3404</td><td>32</td><td>60</td><td>22</td><td>40</td><td>54</td><td>100</td><td>4</td><td>B</td>                </tr>
+                                <tr>
+                  <td>BHS3401</td><td>32</td><td>60</td><td>24</td><td>40</td><td>56</td><td>100</td><td>2</td><td>B+</td>                </tr>
+                                <tr>
+                  <td>BCS3451</td><td>50</td><td>60</td><td>35</td><td>40</td><td>85</td><td>100</td><td>1</td><td>A+</td>                </tr>
+                                <tr>
+                  <td>BCS3452</td><td>48</td><td>60</td><td>35</td><td>40</td><td>83</td><td>100</td><td>1</td><td>A+</td>                </tr>
+                                <tr>
+                  <td>BCS3453</td><td>50</td><td>60</td><td>24</td><td>40</td><td>74</td><td>100</td><td>1</td><td>A</td>                </tr>
+                                <tr>
+                  <td>BCS3454</td><td>52</td><td>60</td><td>24</td><td>40</td><td>76</td><td>100</td><td>1</td><td>A+</td>                </tr>
+                                <tr>
+                  <td>GP3401</td><td></td><td></td><td>72</td><td>100</td><td>72</td><td>100</td><td>1</td><td>A</td>                </tr>
+                               <tr style="font-weight:bold;">
+                  <td>Total</td><td>370</td><td>600</td><td>334</td><td>500</td><td> 704</td><td>1100</td><td></td><td></td>                 </tr>
+                                <tr style="text-align:center">
+                  <td colspan="3">Credit Theory</td><td colspan="3">Credit Practical</td><td colspan="3" rowspan="2">SGPA</td>
+                </tr>
+                <tr style="text-align:center">
+                  <td colspan="2">Obt.</td><td>Max</td> <td colspan="2">Obt.</td><td>Max</td>
+                </tr>
+                <tr style="text-align:center">
+                  <td colspan="2">21</td><td>21</td> <td colspan="2"> 5</td><td> 5</td>
+                  <td colspan="3">100.6</td>
+                </tr>
+                                  </tbody>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  <div><span style="font-weight:bold;">Carry Over Paper(s):</span>BCS3303,<br></div>
+    <div><span style="font-weight:bold;"> Carry Over Paper(s) Detail:</span> </div>
+	
+	<div> </div>
+			  
+        <table class="table tb-print table-bordered tb-mrsheet" style="margin-bottom: 0px; border-collapse:collapse">
+        <tr>
+          <td>
+          <table class="table table-bordered" style="margin-bottom: 0px; border-collapse:collapse" >
+              <thead>
+                <tr style="font-weight:bold;text-align:center;">
+                 <th>Result</th><th>Obt. Marks</th><th>Max. Marks</th>
+                </tr>
+                
+              </thead>
+              <tbody>
+              <tr style="font-weight:bold; text-align:center;">
+                <td>PCP</td><td>1380</td><td>2200</td>
+              </tr>
+                                </tbody>
+            </table></td>
+          <td>
+            <table class="table table-bordered" style="margin-bottom: 0px; border-collapse:collapse" >
+              <thead>
+                
+                <tr>
+                 <th>Credit</th><th>Obt.</th><th>Max.</th>
+                </tr>
+                
+              </thead>
+              <tbody>
+              <tr>
+                <td>Theory</td> <td>38</td> <td>42</td>
+              </tr>
+              <tr>
+                <td>Practical</td> <td>10</td> <td>10</td>
+              </tr>
+                  </tbody>
+            </table>
+          </td>
+        </tr>
+      </table>
+       </div>
+<div style="text-align:center;">Date of Declaration of Result: 18/07/2023 
+<!---<div>
+ <a href="img/Student_Grievance_Form.pdf" target="_blank" style="background: blue;
+    color: white;
+    position: relative;
+    top: 10px;
+    padding: 9px;
+    border-radius: 20px;">Grievance Form</a>
+ </div>---> 
+</div> 
+<section class="ft-prnt" style="margin-top: 10px">
+  <footer class="ft container">
+    <div class="col-sm-12">
+      
+   <br>
+     <b>Note:</b> University does not own for the errors or omissions, if any in this statement. The marks in the tabulation register of the university are final.<br>
+Final year students with PCP status in any year. Please refer to your particular year result for carryover paper code.<br>
+In case of result “ fail” carry over paper(s) is to depict the paper in which student has failed.<br>
+<b>ESE:</b> End semester exam<br>
+<b>CIA:</b> Continuous internal assessment.<br>
+<b>E:</b> Marks Obtained in main exam. B: Marks Obtained in carry over exam.<br>
+<b>PCP:</b> Promoted with carry over Paper(s).<br>
+<b>PWG:</b> Pass with grace.<br>
+<b>Div Not awarded:</b> Due to Carry Over Paper(s) in  Current/ Previous years<br>
+<b>SGPA:</b> Semester Grade Point Average<br>
+<b>CGPA:</b> Cumulative Grade Point Average<br>
+
+     
+
+
+      
+      
+ 
+    </div>
+  </footer>
+</section>
+
+</body>
+</html>
